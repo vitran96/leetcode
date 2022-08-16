42b34eea5b37ba6509ed984319b393c9e1952bec
vitran96 <tranuytrieuvi@gmail.com>
2022-08-16 07:27:38 +0700

    reverse int add comment

--- java/src/main/java/tech/chris/leetcode/ReverseInt.java
+++ java/src/main/java/tech/chris/leetcode/ReverseInt.java
@@ -2,6 +2,10 @@ package tech.chris.leetcode;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+/*
+ * Given a signed 32-bit integer x, return x with its digits reversed.
+ * If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.s
+ */
 public final class ReverseInt implements ISolution {
     public static int solution(int x) {
         // TODO: implement

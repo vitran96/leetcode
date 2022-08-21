f9f5e2170a9282687170e2257a948ead6b7bdbc2
vitran96 <tranuytrieuvi@gmail.com>
2022-08-21 08:03:37 +0700

    validparentheses solution

--- java/src/main/java/tech/chris/leetcode/ValidParentheses.java
+++ java/src/main/java/tech/chris/leetcode/ValidParentheses.java
@@ -1,6 +1,6 @@
 package tech.chris.leetcode;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import java.util.Stack;
 
 //Given a string s containing just the characters'(',')','{','}','['and']',determine if the input string is valid.
 //
@@ -9,8 +9,34 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 //    Open brackets must be closed by the same type of brackets.
 //    Open brackets must be closed in the correct order.
 public class ValidParentheses implements ISolution {
-    public static boolean solution(String str) {
-        // TODO:
-        throw new NotImplementedException();
+    private static String getExpectedString(String str) {
+        switch (str) {
+            case "{":
+                return "}";
+            case "[":
+                return "]";
+            case "(":
+                return ")";
+            default:
+                return "";
+        }
+    }
+
+    public static boolean solution(String input) {
+        Stack<String> parentheses = new Stack<>();
+        String[] strings = input.split("");
+        for (String str : strings) {
+            String expectedBracket = getExpectedString(str);
+            if (expectedBracket.equals("")) {
+                String lastBracket = parentheses.pop();
+                if (!lastBracket.equals(str)) {
+                    return false;
+                }
+            } else {
+                parentheses.push(expectedBracket);
+            }
+        }
+
+        return parentheses.empty();
     }
 }

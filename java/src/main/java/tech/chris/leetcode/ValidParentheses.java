c8f53beadc9d221191c2f7eeeae89badac1265ec
vitran96 <tranuytrieuvi@gmail.com>
2022-08-21 07:52:27 +0700

    validparentheses - init

---
+++ java/src/main/java/tech/chris/leetcode/ValidParentheses.java
@@ -0,0 +1,16 @@
+package tech.chris.leetcode;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+//Given a string s containing just the characters'(',')','{','}','['and']',determine if the input string is valid.
+//
+//    An input string is valid if:
+//
+//    Open brackets must be closed by the same type of brackets.
+//    Open brackets must be closed in the correct order.
+public class ValidParentheses implements ISolution {
+    public static boolean solution(String str) {
+        // TODO:
+        throw new NotImplementedException();
+    }
+}

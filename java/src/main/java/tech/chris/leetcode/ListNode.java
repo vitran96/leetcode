f6be5ed8ee34e358cd26316b81402f4ba8072ec4
vitran96 <tranuytrieuvi@gmail.com>
2022-08-22 07:52:05 +0700

    merge2srotedlist - solution

--- java/src/main/java/tech/chris/leetcode/ListNode.java
+++ java/src/main/java/tech/chris/leetcode/ListNode.java
@@ -1,18 +1,38 @@
 package tech.chris.leetcode;
 
+import java.util.Objects;
+
 public class ListNode {
     public int val;
     public ListNode next;
 
-    public ListNode() {
+    public ListNode () {
     }
 
-    public ListNode(int val) {
+    public ListNode (int val) {
         this.val = val;
     }
 
-    public ListNode(int val, ListNode next) {
+    public ListNode (int val, ListNode next) {
         this.val = val;
         this.next = next;
+    }
+
+    @Override
+    public boolean equals (Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ListNode listNode = (ListNode) o;
+
+        if (val != listNode.val) return false;
+        return Objects.equals(next, listNode.next);
+    }
+
+    @Override
+    public int hashCode () {
+        int result = val;
+        result = 31 * result + (next != null ? next.hashCode() : 0);
+        return result;
     }
 }
--- java/src/main/java/tech/chris/leetcode/Merge2SortedList.java
+++ java/src/main/java/tech/chris/leetcode/Merge2SortedList.java
@@ -1,13 +1,49 @@
 package tech.chris.leetcode;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 //You are given the heads of two sorted linked lists list1 and list2.
 //Merge the two lists in a one sorted list.The list should be made by splicing together the nodes of the first two lists.
 //Return the head of the merged linked list.
 public class Merge2SortedList {
     public ListNode mergeTwoLists (ListNode list1, ListNode list2) {
-        // TODO:
-        throw new NotImplementedException();
+        ListNode top = null;
+        ListNode current = null;
+        while (list1 != null && list2 != null) {
+            int val;
+            if (list1.val <= list2.val) {
+                val = list1.val;
+                list1 = list1.next;
+            } else {
+                val = list2.val;
+                list2 = list2.next;
+            }
+
+            if (top == null) {
+                top = new ListNode(val);
+                current = top;
+            } else {
+                current.next = new ListNode(val);
+                current = current.next;
+            }
+        }
+
+        if (list1 == null && list2 == null) {
+            return top;
+        }
+
+        ListNode nonNullList = list1 == null ? list2 : list1;
+        while (nonNullList != null) {
+            if (top == null) {
+                top = new ListNode(nonNullList.val);
+                current = top;
+            } else {
+                current.next = new ListNode(nonNullList.val);
+                current = current.next;
+            }
+
+            nonNullList = nonNullList.next;
+        }
+
+
+        return top;
     }
 }

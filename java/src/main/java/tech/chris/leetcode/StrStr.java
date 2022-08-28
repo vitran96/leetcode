84819d84437e25b49b1e739486d8df2d19929ea1
vitran96 <tranuytrieuvi@gmail.com>
2022-08-29 06:49:20 +0700

    strstr - solution

--- java/src/main/java/tech/chris/leetcode/StrStr.java
+++ java/src/main/java/tech/chris/leetcode/StrStr.java
@@ -1,13 +1,10 @@
 package tech.chris.leetcode;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 // Given two strings needle and haystack,
 // return the index of the first occurrence of needle in haystack,
 // or -1 if needle is not part of haystack.
 public class StrStr implements ISolution {
     public int strStr (String haystack, String needle) {
-        // TODO:
-        throw new NotImplementedException();
+        return haystack.indexOf(needle);
     }
 }

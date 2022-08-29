b266b600bc23f2c996994839cefb0f7d1864fc4a
vitran96 <tranuytrieuvi@gmail.com>
2022-08-29 07:21:56 +0700

    plusone - solution

--- java/src/main/java/tech/chris/leetcode/PlusOne.java
+++ java/src/main/java/tech/chris/leetcode/PlusOne.java
@@ -1,7 +1,5 @@
 package tech.chris.leetcode;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 // You are given a large integer represented as an integer array digits,
 // where each digits[i]is the ith digit of the integer.
 // The digits are ordered from most significant to least significant in left-to-right order.
@@ -10,7 +8,29 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 // Increment the large integer by one and return the resulting array of digits.
 public class PlusOne implements ISolution {
     public int[] plusOne (int[] digits) {
-        // TODO:
-        throw new NotImplementedException();
+        boolean hasRemain = false;
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (i == digits.length - 1 || hasRemain) {
+                digits[i]++;
+            }
+
+            if (digits[i] >= 10) {
+                digits[i] -= 10;
+                hasRemain = true;
+            } else {
+                hasRemain = false;
+            }
+        }
+
+        int[] results;
+        if (hasRemain) {
+            results = new int[digits.length + 1];
+            System.arraycopy(digits, 0, results, 1, digits.length);
+            results[0] = 1;
+        } else {
+            results = digits;
+        }
+
+        return results;
     }
 }

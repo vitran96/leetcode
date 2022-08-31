eb573f85dbd35878a839c425505859f7015955cd
vitran96 <tranuytrieuvi@gmail.com>
2022-08-31 07:01:13 +0700

    sqrt - solution

--- java/src/main/java/tech/chris/leetcode/SqrtX.java
+++ java/src/main/java/tech/chris/leetcode/SqrtX.java
@@ -1,7 +1,5 @@
 package tech.chris.leetcode;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 //Given a non-negative integer x,compute and return the square root of x.
 //
 //Since the return type is an integer,the decimal digits are truncated,and only the integer part of the result is returned.
@@ -9,7 +7,22 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 //Note:You are not allowed to use any built-in exponent function or operator,such as pow(x,0.5)or x**0.5.
 public class SqrtX implements ISolution {
     public int mySqrt (int x) {
-        // TODO:
-        throw new NotImplementedException();
+        if (x == 0) {
+            return 0;
+        } else if (x <= 3) {
+            return 1;
+        }
+
+        int result = 2;
+        while (true) {
+            final long next = result + 1;
+            if (next * next > x) {
+                break;
+            }
+
+            result = (int) next;
+        }
+
+        return result;
     }
 }
--- java/src/test/java/tech/chris/leetcode/SqrtXTest.java
+++ java/src/test/java/tech/chris/leetcode/SqrtXTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 public class SqrtXTest {
 
     @ParameterizedTest
-    @CsvSource({"4,2", "8,2"})
+    @CsvSource({"4,2", "8,2", "0,0", "1,1", "9,3", "10,3", "99,9", "2,1", "3,1", "2147395600,46340"})
     public void test (int input, int expected) {
         Assertions.assertEquals(expected, new SqrtX().mySqrt(input));
     }

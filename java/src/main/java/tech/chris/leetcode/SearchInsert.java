3cc21c4f10123ce76ee1c9144bb85627ebb18e6d
vitran96 <tranuytrieuvi@gmail.com>
2022-09-01 06:50:24 +0700

    searchinsert - init

---
+++ java/src/main/java/tech/chris/leetcode/SearchInsert.java
@@ -0,0 +1,10 @@
+package tech.chris.leetcode;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class SearchInsert {
+    public int searchInsert (int[] nums, int target) {
+        // TODO:
+        throw new NotImplementedException();
+    }
+}
---
+++ java/src/test/java/tech/chris/leetcode/SearchInsertTest.java
@@ -0,0 +1,17 @@
+package tech.chris.leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class SearchInsertTest {
+
+    @ParameterizedTest
+    @CsvSource({"'1,3,5,6',5,2", "'1,3,5,6',2,1", "'1,3,5,6',7,4"})
+    public void test (@ConvertWith(IntArrayConverter.class) int[] input1,
+                      int input2,
+                      int expected) {
+        Assertions.assertEquals(expected, new SearchInsert().searchInsert(input1, input2));
+    }
+}

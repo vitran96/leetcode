a5b0d528a596da24f856d93531c8cd305884b406
vitran96 <tranuytrieuvi@gmail.com>
2022-09-01 07:15:32 +0700

    searchinsert - solution

--- java/src/main/java/tech/chris/leetcode/SearchInsert.java
+++ java/src/main/java/tech/chris/leetcode/SearchInsert.java
@@ -1,10 +1,30 @@
 package tech.chris.leetcode;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
+//Given a sorted array of distinct integers and a target value,return the index if the target is found.If not,return the index where it would be if it were inserted in order.
+//
+//You must write an algorithm with O(log n)runtime complexity.
 public class SearchInsert {
     public int searchInsert (int[] nums, int target) {
-        // TODO:
-        throw new NotImplementedException();
+        int l = 0;
+        int r = nums.length - 1;
+
+        int mid = 0;
+
+        while (l <= r) {
+            mid = (l + r) / 2;
+            if (nums[mid] == target) {
+                break;
+            } else if (nums[mid] > target) {
+                r = mid - 1;
+            } else if (nums[mid] < target) {
+                l = mid + 1;
+            }
+        }
+
+        if (nums[mid] < target) {
+            mid++;
+        }
+
+        return mid;
     }
 }
--- java/src/test/java/tech/chris/leetcode/SearchInsertTest.java
+++ java/src/test/java/tech/chris/leetcode/SearchInsertTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 public class SearchInsertTest {
 
     @ParameterizedTest
-    @CsvSource({"'1,3,5,6',5,2", "'1,3,5,6',2,1", "'1,3,5,6',7,4"})
+    @CsvSource({"'1,3,5,6',5,2", "'1,3,5,6',2,1", "'1,3,5,6',7,4", "'1,3,5,6',0,0", "'1,3',2,1"})
     public void test (@ConvertWith(IntArrayConverter.class) int[] input1,
                       int input2,
                       int expected) {

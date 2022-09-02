7a38fddcb7bac98e3feff44b98f3a85d2016af48
vitran96 <tranuytrieuvi@gmail.com>
2022-09-02 08:04:23 +0700

    climbingstairs - solution

--- java/src/main/java/tech/chris/leetcode/ClimbingStairs.java
+++ java/src/main/java/tech/chris/leetcode/ClimbingStairs.java
@@ -1,13 +1,16 @@
 package tech.chris.leetcode;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 //You are climbing a staircase.It takes n steps to reach the top.
 //
 //Each time you can either climb 1or 2steps.In how many distinct ways can you climb to the top?
+//
+// 1 <= n <= 45
 public class ClimbingStairs {
     public int climbStairs (int n) {
-        // TODO:
-        throw new NotImplementedException();
+        if (n <= 2) {
+            return n;
+        }
+
+        return climbStairs(n - 2) + climbStairs(n - 1);
     }
 }
--- java/src/test/java/tech/chris/leetcode/ClimbingStairsTest.java
+++ java/src/test/java/tech/chris/leetcode/ClimbingStairsTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 public class ClimbingStairsTest {
 
     @ParameterizedTest
-    @CsvSource({"2,2", "3,3"})
+    @CsvSource({"1,1", "2,2", "3,3", "4,5", "5,8", "6,13", "45,1836311903"})
     public void test (int input, int expected) {
         Assertions.assertEquals(expected, new ClimbingStairs().climbStairs(input));
     }

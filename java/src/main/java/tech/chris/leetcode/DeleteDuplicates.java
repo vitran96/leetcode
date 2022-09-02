e1cfc507719ec655a2847a9f122c22c892ba633b
vitran96 <tranuytrieuvi@gmail.com>
2022-09-02 08:32:38 +0700

    delete duplicates - solution

--- java/src/main/java/tech/chris/leetcode/DeleteDuplicates.java
+++ java/src/main/java/tech/chris/leetcode/DeleteDuplicates.java
@@ -1,10 +1,28 @@
 package tech.chris.leetcode;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class DeleteDuplicates {
     public ListNode deleteDuplicates (ListNode head) {
-        // TODO:
-        throw new NotImplementedException();
+        Set<Integer> nums = new HashSet<>();
+        ListNode newHead = null;
+        ListNode current = null;
+
+        while (head != null) {
+            if (newHead == null) {
+                newHead = new ListNode(head.val);
+                current = newHead;
+            } else if (!nums.contains(head.val)) {
+                ListNode next = new ListNode(head.val);
+                current.next = next;
+                current = next;
+            }
+
+            nums.add(head.val);
+            head = head.next;
+        }
+
+        return newHead;
     }
 }

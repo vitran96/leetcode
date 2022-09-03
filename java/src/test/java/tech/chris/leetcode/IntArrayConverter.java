c5ad7d3861777be3bb55fd4d69a2aca0c5ca2fd2
vitran96 <tranuytrieuvi@gmail.com>
2022-09-03 07:07:28 +0700

    update IntArrayConverter

--- java/src/test/java/tech/chris/leetcode/IntArrayConverter.java
+++ java/src/test/java/tech/chris/leetcode/IntArrayConverter.java
@@ -9,6 +9,10 @@ public class IntArrayConverter extends SimpleArgumentConverter {
     @Override
     protected Object convert (Object source, Class<?> targetType) throws ArgumentConversionException {
         if (source instanceof String && int[].class.isAssignableFrom(targetType)) {
+            if (((String) source).isEmpty()) {
+                return new int[0];
+            }
+
             return Arrays.stream(((String) source).split(",")).mapToInt(Integer::parseInt).toArray();
         } else {
             throw new IllegalArgumentException("Conversion from " + source.getClass() + " to "

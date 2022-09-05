edc6fbc713d12b1608b85cfb2d6772472326896b
vitran96 <tranuytrieuvi@gmail.com>
2022-09-06 06:27:02 +0700

    same tree - init

---
+++ java/src/main/java/tech/chris/leetcode/SameTree.java
@@ -0,0 +1,10 @@
+package tech.chris.leetcode;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class SameTree {
+    public boolean isSameTree (TreeNode p, TreeNode q) {
+        // TODO:
+        throw new NotImplementedException();
+    }
+}

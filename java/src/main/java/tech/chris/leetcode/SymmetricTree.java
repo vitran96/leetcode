1fb99de41d4c5a5deec507f0ee790722b468a1b5
vitran96 <tranuytrieuvi@gmail.com>
2022-09-10 05:55:42 +0700

    symmetric tree - init

---
+++ java/src/main/java/tech/chris/leetcode/SymmetricTree.java
@@ -0,0 +1,12 @@
+package tech.chris.leetcode;
+
+//Given the root of a binary tree,check whether it is a mirror of itself(i.e.,symmetric around its center).
+
+// The number of nodes in the tree is in the range[1,1000].
+// -100 <= Node.val <= 100
+public class SymmetricTree {
+    public boolean isSymmetric (TreeNode root) {
+        // TODO
+        return false;
+    }
+}
---
+++ java/src/test/java/tech/chris/leetcode/SymmetricTreeTest.java
@@ -0,0 +1,15 @@
+package tech.chris.leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class SymmetricTreeTest {
+
+    @ParameterizedTest
+    @CsvSource({"'1,2,2,3,4,4,3',true", "'1,2,2,null,3,null,3',false"})
+    public void test (@ConvertWith(TreeNodeConverter.class) TreeNode input, boolean expected) {
+        Assertions.assertEquals(expected, new SymmetricTree().isSymmetric(input));
+    }
+}

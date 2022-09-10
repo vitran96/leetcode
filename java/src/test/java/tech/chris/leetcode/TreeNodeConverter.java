2dcec052904ed87e5139a560a86b4848defd7eaa
vitran96 <tranuytrieuvi@gmail.com>
2022-09-10 07:07:41 +0700

    try fix tree node converter

--- java/src/test/java/tech/chris/leetcode/TreeNodeConverter.java
+++ java/src/test/java/tech/chris/leetcode/TreeNodeConverter.java
@@ -4,15 +4,17 @@ import org.junit.jupiter.params.converter.ArgumentConversionException;
 import org.junit.jupiter.params.converter.SimpleArgumentConverter;
 
 public final class TreeNodeConverter extends SimpleArgumentConverter {
-    private static TreeNode convert (String[] values, int index, int level) {
+    private static TreeNode convert (String[] values, int index, int level, int extraStep) {
         if (index >= values.length || values[index].equals("null")) {
             return null;
         }
 
         TreeNode currentRoot = new TreeNode(Integer.parseInt(values[index]));
         int nextLeftIndex = index + ((int) Math.pow(2, level));
-        currentRoot.left = convert(values, nextLeftIndex, level + 1);
-        currentRoot.right = convert(values, 1 + nextLeftIndex, currentRoot.left == null ? level : level + 1);
+        currentRoot.left = convert(values, nextLeftIndex + extraStep, level + 1, 0);
+//        currentRoot.right = convert(values, 1 + nextLeftIndex, currentRoot.left == null ? level : level + 1,
+//                                    currentRoot.left == null ? 0 : 1);
+        currentRoot.right = convert(values, 1 + nextLeftIndex + extraStep, level + 1, currentRoot.left == null ? 0 : 1);
 
         return currentRoot;
     }
@@ -25,7 +27,8 @@ public final class TreeNodeConverter extends SimpleArgumentConverter {
             }
 
             String[] strs = ((String) source).split(",");
-            return convert(strs, 0, 0);
+            // TODO: fix
+            return convert(strs, 0, 0, 0);
         } else {
             throw new IllegalArgumentException(
                 "Conversion from " + source.getClass() + " to " + targetType + " not supported.");

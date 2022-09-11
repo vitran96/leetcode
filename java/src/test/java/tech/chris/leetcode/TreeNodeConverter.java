c641ff5dba3adc0a90b56e1fae5a9060b87c307a
vitran96 <tranuytrieuvi@gmail.com>
2022-09-11 13:25:54 +0700

    fix tree node converter

--- java/src/test/java/tech/chris/leetcode/TreeNodeConverter.java
+++ java/src/test/java/tech/chris/leetcode/TreeNodeConverter.java
@@ -3,32 +3,56 @@ package tech.chris.leetcode;
 import org.junit.jupiter.params.converter.ArgumentConversionException;
 import org.junit.jupiter.params.converter.SimpleArgumentConverter;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public final class TreeNodeConverter extends SimpleArgumentConverter {
-    private static TreeNode convert (String[] values, int index, int level, int extraStep) {
-        if (index >= values.length || values[index].equals("null")) {
+
+    private static TreeNode makeTreeNode (final String[] strings, final int index) {
+        if (index >= strings.length) {
             return null;
         }
 
-        TreeNode currentRoot = new TreeNode(Integer.parseInt(values[index]));
-        int nextLeftIndex = index + ((int) Math.pow(2, level));
-        currentRoot.left = convert(values, nextLeftIndex + extraStep, level + 1, 0);
-//        currentRoot.right = convert(values, 1 + nextLeftIndex, currentRoot.left == null ? level : level + 1,
-//                                    currentRoot.left == null ? 0 : 1);
-        currentRoot.right = convert(values, 1 + nextLeftIndex + extraStep, level + 1, currentRoot.left == null ? 0 : 1);
+        return str2TreeNode(strings[index]);
+    }
 
-        return currentRoot;
+    private static TreeNode str2TreeNode (final String s) {
+        if (s == null || s.equals("null")) {
+            return null;
+        }
+
+        return new TreeNode(Integer.parseInt(s));
     }
 
     @Override
-    protected Object convert (Object source, Class<?> targetType) throws ArgumentConversionException {
+    protected Object convert (final Object source, final Class<?> targetType) throws ArgumentConversionException {
         if (source instanceof String && TreeNode.class.isAssignableFrom(targetType)) {
             if (((String) source).length() == 0) {
                 return null;
             }
 
-            String[] strs = ((String) source).split(",");
-            // TODO: fix
-            return convert(strs, 0, 0, 0);
+            final String[] strings = ((String) source).split(",");
+            int n = 0;
+            if (strings.length == 0 || strings[n].equals("null")) {
+                return null;
+            }
+
+            final TreeNode root = makeTreeNode(strings, n);
+            final Queue<TreeNode> queue = new LinkedList<>();
+            queue.add(root);
+            while (queue.size() != 0) {
+                TreeNode current = queue.poll();
+                if (current == null) {
+                    continue;
+                }
+                current.left = makeTreeNode(strings, 2 * n + 1);
+                queue.add(current.left);
+                current.right = makeTreeNode(strings, 2 * n + 2);
+                queue.add(current.right);
+                n++;
+            }
+
+            return root;
         } else {
             throw new IllegalArgumentException(
                 "Conversion from " + source.getClass() + " to " + targetType + " not supported.");

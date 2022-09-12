f085417b14d8ed01a0c1071e72b6c630b76d247a
vitran96 <tranuytrieuvi@gmail.com>
2022-09-13 06:37:07 +0700

    balanced binary tree

---
+++ java/src/main/java/tech/chris/leetcode/BalancedBinaryTree.java
@@ -0,0 +1,17 @@
+package tech.chris.leetcode;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+//Given a binary tree,determine if it is height-balanced.
+//For this problem,a height-balanced binary tree is defined as:
+//    a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
+//
+//
+// The number of nodes in the tree is in the range [0,5000].
+// -104 <= Node.val <= 104
+public class BalancedBinaryTree {
+    public boolean isBalanced (TreeNode root) {
+        // TODO:
+        throw new NotImplementedException();
+    }
+}

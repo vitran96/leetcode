1011897560ce241a3362b10711c2bd5291b72373
vitran96 <tranuytrieuvi@gmail.com>
2022-09-14 06:40:12 +0700

    pascal triangle - init

---
+++ java/src/main/java/tech/chris/leetcode/PascalsTriangle.java
@@ -0,0 +1,17 @@
+package tech.chris.leetcode;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.List;
+
+// Given an integer numRows, return the first numRows of Pascal's triangle.
+//
+// In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+//
+// 1 <= numRows <= 30
+public class PascalsTriangle {
+    public List<List<Integer>> generate (int numRows) {
+        // TODO:
+        throw new NotImplementedException();
+    }
+}
---
+++ java/src/test/java/tech/chris/leetcode/PascalsTriangleTest.java
@@ -0,0 +1,14 @@
+package tech.chris.leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class PascalsTriangleTest {
+
+    @ParameterizedTest
+    @CsvSource({"5,[[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]", "1,[[1]]"})
+    public void test (int numRows, String expected) {
+        Assertions.assertEquals(expected, (new PascalsTriangle().generate(numRows)).toString());
+    }
+}

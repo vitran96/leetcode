33d35a5288bd942435842d78ba9931e4692eed04
vitran96 <tranuytrieuvi@gmail.com>
2022-09-14 06:19:12 +0700

    path sum - solution

--- java/src/main/java/tech/chris/leetcode/PathSum.java
+++ java/src/main/java/tech/chris/leetcode/PathSum.java
@@ -1,7 +1,5 @@
 package tech.chris.leetcode;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 // Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that
 // adding up all the values along the path equals targetSum.
 // A leaf is a node with no children.
@@ -11,7 +9,15 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 // -1000 <= targetSum <= 1000
 public class PathSum {
     public boolean hasPathSum (TreeNode root, int targetSum) {
-        // TODO:
-        throw new NotImplementedException();
+        if (root == null) {
+            return false;
+        }
+
+        targetSum -= root.val;
+        if (targetSum == 0) {
+            return root.left == null && root.right == null;
+        }
+
+        return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);
     }
 }
--- java/src/test/java/tech/chris/leetcode/PathSumTest.java
+++ java/src/test/java/tech/chris/leetcode/PathSumTest.java
@@ -8,7 +8,8 @@ import org.junit.jupiter.params.provider.CsvSource;
 public class PathSumTest {
 
     @ParameterizedTest
-    @CsvSource({"'5,4,8,11,null,13,4,7,2,null,null,null,1',22,true", "'1,2,3',5,false", "'',0,false"})
+    @CsvSource({"'5,4,8,11,null,13,4,7,2,null,null,null,1',22,true", "'1,2,3',5,false", "'',0,false",
+                "'1,2,3,4',3,false"})
     public void test (@ConvertWith(TreeNodeConverter.class) TreeNode root, int targetSum, boolean expected) {
         Assertions.assertEquals(expected, new PathSum().hasPathSum(root, targetSum));
     }

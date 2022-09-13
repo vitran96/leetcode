249912731d4f85d422c66603810413761949cfc9
vitran96 <tranuytrieuvi@gmail.com>
2022-09-13 07:37:41 +0700

    min depth of binary tree - solution

--- java/src/main/java/tech/chris/leetcode/MinDepthOfBinaryTree.java
+++ java/src/main/java/tech/chris/leetcode/MinDepthOfBinaryTree.java
@@ -1,7 +1,5 @@
 package tech.chris.leetcode;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 // Given a binary tree,find its minimum depth.
 // The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
 // Note:A leaf is a node with no children.
@@ -11,7 +9,17 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 // -1000 <= Node.val <= 1000
 public class MinDepthOfBinaryTree {
     public int minDepth (TreeNode root) {
-        // TODO:
-        throw new NotImplementedException();
+        if (root == null) {
+            return 0;
+        }
+
+        final int leftHeight = minDepth(root.left);
+        final int rightHeight = minDepth(root.right);
+
+        if (leftHeight == 0 || rightHeight == 0) {
+            return 1 + Math.max(leftHeight, rightHeight);
+        }
+
+        return 1 + Math.min(leftHeight, rightHeight);
     }
 }
--- java/src/test/java/tech/chris/leetcode/MinDepthOfBinaryTreeTest.java
+++ java/src/test/java/tech/chris/leetcode/MinDepthOfBinaryTreeTest.java
@@ -9,7 +9,7 @@ public class MinDepthOfBinaryTreeTest {
 
     @ParameterizedTest
     @CsvSource({"'3,9,20,null,null,15,7',2", "'2,null,3,null,4,null,5,null,6',5", "'2,1,3,null,4,null,5,null,6',3",
-                "'',0"})
+                "'',0", "'1',1"})
     public void test (@ConvertWith(TreeNodeConverter.class) TreeNode input, int expected) {
         Assertions.assertEquals(expected, new MinDepthOfBinaryTree().minDepth(input));
     }

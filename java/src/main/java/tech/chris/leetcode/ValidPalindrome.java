0852cfa2dcaefd887a8ba8aa3cc1a62fd68972ce
vitran96 <tranuytrieuvi@gmail.com>
2022-09-17 07:44:06 +0700

    valid palindrome - solution

--- java/src/main/java/tech/chris/leetcode/ValidPalindrome.java
+++ java/src/main/java/tech/chris/leetcode/ValidPalindrome.java
@@ -1,10 +1,25 @@
 package tech.chris.leetcode;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters,
+// it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+// Given a string s, return true if it is a palindrome, or false otherwise.
+// 1 <= s.length <= 2*10^5
+// s consists only of printable ASCII characters.
 public class ValidPalindrome {
     public boolean isPalindrome (String s) {
-        // TODO:
-        throw new NotImplementedException();
+        String filteredString = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+
+        int l = 0;
+        int r = filteredString.length() - 1;
+
+        while (l <= r) {
+            if (filteredString.charAt(l) != filteredString.charAt(r)) {
+                return false;
+            }
+
+            l++;
+            r--;
+        }
+        return true;
     }
 }

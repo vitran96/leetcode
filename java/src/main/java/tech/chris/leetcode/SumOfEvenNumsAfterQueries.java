ea9276dfaf28adfc8b65402ae1409aa6f849f31f
vitran96 <tranuytrieuvi@gmail.com>
2022-09-22 05:45:42 +0700

    sum of even nums after queries - solution - slow

--- java/src/main/java/tech/chris/leetcode/SumOfEvenNumsAfterQueries.java
+++ java/src/main/java/tech/chris/leetcode/SumOfEvenNumsAfterQueries.java
@@ -1,7 +1,5 @@
 package tech.chris.leetcode;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 // You are given an integer array nums and an array queries where queries[i] = [val, index].
 //
 // For each query i, first, apply nums[index] = nums[index] + val, then print the sum of the even values of nums.
@@ -15,7 +13,24 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 //    0 <= index < nums.length
 public class SumOfEvenNumsAfterQueries {
     public int[] sumEvenAfterQueries (int[] nums, int[][] queries) {
-        // TODO:
-        throw new NotImplementedException();
+        int[] result = new int[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+            final int[] query = queries[i];
+            final int val = query[0];
+            final int index = query[1];
+
+            nums[index] = nums[index] + val;
+            int sumOfEvenNums = 0;
+            for (final int num : nums) {
+                if (num % 2 == 0) {
+                    sumOfEvenNums += num;
+                }
+            }
+
+            result[i] = sumOfEvenNums;
+        }
+
+        return result;
     }
 }

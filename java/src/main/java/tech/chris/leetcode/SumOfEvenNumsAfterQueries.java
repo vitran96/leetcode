73d26fe51e2939ea0777a64ebd172caff0427f72
vitran96 <tranuytrieuvi@gmail.com>
2022-09-22 05:33:45 +0700

    sum of even nums after queries - init

---
+++ java/src/main/java/tech/chris/leetcode/SumOfEvenNumsAfterQueries.java
@@ -0,0 +1,21 @@
+package tech.chris.leetcode;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+// You are given an integer array nums and an array queries where queries[i] = [val, index].
+//
+// For each query i, first, apply nums[index] = nums[index] + val, then print the sum of the even values of nums.
+//
+// Return an integer array answer where answer[i] is the answer to the i th query.
+//
+//    1 <= nums.length <= 10^4
+//    -10^4 <= nums[i] <= 10^4
+//    1 <= queries.length <= 10^4
+//    -10^4 <= val <= 10^4
+//    0 <= index < nums.length
+public class SumOfEvenNumsAfterQueries {
+    public int[] sumEvenAfterQueries (int[] nums, int[][] queries) {
+        // TODO:
+        throw new NotImplementedException();
+    }
+}
---
+++ java/src/test/java/tech/chris/leetcode/SumOfEvenNumsAfterQueriesTest.java
@@ -0,0 +1,23 @@
+package tech.chris.leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class SumOfEvenNumsAfterQueriesTest {
+    static Stream<Arguments> provideArgument () {
+        return Stream.of(Arguments.of(new int[]{1, 2, 3, 4},
+                                      new int[][]{new int[]{1, 0}, new int[]{-3, 1}, new int[]{-4, 0}, new int[]{2, 3}},
+                                      new int[]{8, 6, 2, 4}),
+                         Arguments.of(new int[]{1}, new int[][]{new int[]{4, 0}}, new int[]{0}));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArgument")
+    public void test (int[] nums, int[][] queries, int[] expected) {
+        Assertions.assertEquals(new SumOfEvenNumsAfterQueries().sumEvenAfterQueries(nums, queries), expected);
+    }
+}

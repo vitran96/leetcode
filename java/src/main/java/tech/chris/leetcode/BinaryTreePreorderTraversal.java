43e29fb9d14667497cb3daadff60da8aa89c5fc2
vitran96 <tranuytrieuvi@gmail.com>
2022-09-21 14:40:16 +0700

    binary tree preorder traversal

---
+++ java/src/main/java/tech/chris/leetcode/BinaryTreePreorderTraversal.java
@@ -0,0 +1,16 @@
+package tech.chris.leetcode;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.List;
+
+// Given the root of a binary tree, return the preorder traversal of its nodes' values.
+//
+// The number of nodes in the tree is in the range [0, 100].
+// -100 <= Node.val <= 100
+public class BinaryTreePreorderTraversal {
+    public List<Integer> preorderTraversal (TreeNode root) {
+        // TODO:
+        throw new NotImplementedException();
+    }
+}

498bd41b8c5b7247fe6380275eada5b2799cad36
vitran96 <tranuytrieuvi@gmail.com>
2022-09-23 05:24:30 +0700

    sum of even nums after queries - solution - optimize

--- java/src/main/java/tech/chris/leetcode/SumOfEvenNumsAfterQueries.java
+++ java/src/main/java/tech/chris/leetcode/SumOfEvenNumsAfterQueries.java
@@ -1,8 +1,5 @@
 package tech.chris.leetcode;
 
-import java.util.HashSet;
-import java.util.Set;
-
 // You are given an integer array nums and an array queries where queries[i] = [val, index].
 //
 // For each query i, first, apply nums[index] = nums[index] + val, then print the sum of the even values of nums.
@@ -18,36 +15,28 @@ public class SumOfEvenNumsAfterQueries {
     public int[] sumEvenAfterQueries (int[] nums, int[][] queries) {
         int[] result = new int[queries.length];
 
-        Set<Integer> setOfOddIndex = new HashSet<>();
         int sumOfEvenNums = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] % 2 == 0) {
-                sumOfEvenNums += nums[i];
-            } else {
-                setOfOddIndex.add(i);
+        for (int num : nums) {
+            if (num % 2 == 0) {
+                sumOfEvenNums += num;
             }
         }
 
         for (int i = 0; i < queries.length; i++) {
-            final int[] query = queries[i];
+            final int val = queries[i][0];
+            final int index = queries[i][1];
 
-            final int val = query[0];
-            final int index = query[1];
-
-            final int currentVal = nums[index];
-            nums[index] = val + currentVal;
-
+            // Remove the current nums[index] from sum if nums[index] is even
+            // to avoid nums[index] become odd after query
             if (nums[index] % 2 == 0) {
-                if (setOfOddIndex.remove(index)) {
-                    sumOfEvenNums += nums[index];
-                } else {
-                    sumOfEvenNums += val;
-                }
-            } else {
-                if (!setOfOddIndex.contains(index)) {
-                    sumOfEvenNums -= currentVal;
-                    setOfOddIndex.add(index);
-                }
+                sumOfEvenNums -= nums[index];
+            }
+
+            nums[index] += val;
+
+            // Add nums[index] to the sum if nums[index] is even
+            if (nums[index] % 2 == 0) {
+                sumOfEvenNums += nums[index];
             }
 
             result[i] = sumOfEvenNums;

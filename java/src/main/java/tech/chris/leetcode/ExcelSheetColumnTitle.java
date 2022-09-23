acdaf108e9568ce9b0dd2298eb717379083850a8
vitran96 <tranuytrieuvi@gmail.com>
2022-09-23 07:00:04 +0700

    excel sheet column title - online solution

--- java/src/main/java/tech/chris/leetcode/ExcelSheetColumnTitle.java
+++ java/src/main/java/tech/chris/leetcode/ExcelSheetColumnTitle.java
@@ -1,12 +1,17 @@
 package tech.chris.leetcode;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 // Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
 // 1 <= columnNumber <= 2^31 - 1
 public class ExcelSheetColumnTitle {
+
+    // TODO: should attempt to do this again at some point
     public String convertToTitle (int columnNumber) {
-        // TODO:
-        throw new NotImplementedException();
+        // columnNumber must be shifted by 1
+        // Why?
+        // Image A index is 0 and Z is 25
+        // If not deduce by 1, we can get weird number after a mod
+        // EG: 27 (A) % 26 => 1 (B) -> 1 % 26 => 1 (B) ==> BB -> WRONG
+        // 27 - 1 % 26 => 0 (A) -> 1 - 1 % 26 => 0 (A) ==> AA -> CORRECT
+        return columnNumber == 0 ? "" : convertToTitle(--columnNumber / 26) + (char) ('A' + (columnNumber % 26));
     }
 }
--- java/src/test/java/tech/chris/leetcode/ExcelSheetColumnTitleTest.java
+++ java/src/test/java/tech/chris/leetcode/ExcelSheetColumnTitleTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 public class ExcelSheetColumnTitleTest {
 
     @ParameterizedTest
-    @CsvSource({"1,A", "28,AB", "701,ZY"})
+    @CsvSource({"1,A", "27,AA", "28,AB", "701,ZY", "702,ZZ", "703,AAA", "713,AAK", "18278,ZZZ", "18252,ZYZ"})
     public void test (int input, String expected) {
         Assertions.assertEquals(expected, new ExcelSheetColumnTitle().convertToTitle(input));
     }

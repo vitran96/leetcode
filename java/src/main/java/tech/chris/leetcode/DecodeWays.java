fb13744bb0f8b6199ac0e86f9c8ee97b81a2b9c4
vitran96 <tranuytrieuvi@gmail.com>
2022-10-03 06:45:41 +0700

    decode ways - remove online solution

--- java/src/main/java/tech/chris/leetcode/DecodeWays.java
+++ java/src/main/java/tech/chris/leetcode/DecodeWays.java
@@ -6,28 +6,7 @@ public class DecodeWays {
     // TODO: redo this at some point
     // I don't really get how this work
     public int numDecodings (String s) {
-        return s.length() == 0 ? 0 : numDecodings(s, 0);
-    }
-
-    private int numDecodings (String s, int n) {
-        if (s.length() == n) {
-            return 1;
-        }
-
-        if (s.charAt(0) == '0') {
-            return 0;
-        }
-
-        int res = numDecodings(s, n + 1);
-
-        if (n < s.length() - 1) {
-            if (s.charAt(n) == '1' || (s.charAt(n) == '2' && s.charAt(n + 1) < '7')) {
-                res += numDecodings(s, n + 2);
-            } else if (s.charAt(n) > '2' && s.charAt(n + 1) == '0') {
-                res = 0;
-            }
-        }
-
-        return res;
+        // TODO:
+        return 0;
     }
 }

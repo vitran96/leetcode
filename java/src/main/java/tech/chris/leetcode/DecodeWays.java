59cb26a454cc536468bd070dd87373a5790a5462
vitran96 <tranuytrieuvi@gmail.com>
2022-10-02 08:12:38 +0700

    decode ways - solution - partial

--- java/src/main/java/tech/chris/leetcode/DecodeWays.java
+++ java/src/main/java/tech/chris/leetcode/DecodeWays.java
@@ -1,12 +1,33 @@
 package tech.chris.leetcode;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 // 1 <= s.length <= 100
 // s contains only digits and may contain leading zero(s).
 public class DecodeWays {
-    public int numDecodings(String s) {
-        // TODO:
-        throw new NotImplementedException();
+    // TODO: redo this at some point
+    // I don't really get how this work
+    public int numDecodings (String s) {
+        return s.length() == 0 ? 0 : numDecodings(s, 0);
+    }
+
+    private int numDecodings (String s, int n) {
+        if (s.length() == n) {
+            return 1;
+        }
+
+        if (s.charAt(0) == '0') {
+            return 0;
+        }
+
+        int res = numDecodings(s, n + 1);
+
+        if (n < s.length() - 1) {
+            if (s.charAt(n) == '1' || (s.charAt(n) == '2' && s.charAt(n + 1) < '7')) {
+                res += numDecodings(s, n + 2);
+            } else if (s.charAt(n) > '2' && s.charAt(n + 1) == '0') {
+                res = 0;
+            }
+        }
+
+        return res;
     }
 }
--- java/src/test/java/tech/chris/leetcode/DecodeWaysTest.java
+++ java/src/test/java/tech/chris/leetcode/DecodeWaysTest.java
@@ -7,7 +7,8 @@ import org.junit.jupiter.params.provider.CsvSource;
 public class DecodeWaysTest {
 
     @ParameterizedTest
-    @CsvSource({"12,2", "226,3", "06,0"})
+    @CsvSource({"12,2", "226,3", "06,0", "27,1", "11106,2", "11306,0", "11116,8", "123456,3", "111111,13",
+                "123700532,0","125100135,0"})
     public void test (String input, int expected) {
         Assertions.assertEquals(expected, new DecodeWays().numDecodings(input));
     }

1dd0ac7d172d1500c63da455d9b10199b76086cd
vitran96 <tranuytrieuvi@gmail.com>
2022-10-04 07:18:11 +0700

    majority element - solution 1

--- java/src/main/java/tech/chris/leetcode/MajorityElement.java
+++ java/src/main/java/tech/chris/leetcode/MajorityElement.java
@@ -1,5 +1,8 @@
 package tech.chris.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 // Given an array nums of size n, return the majority element.
 // The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element
 // always exists in the array.
@@ -9,7 +12,20 @@ package tech.chris.leetcode;
 // -10^9 <= nums[i] <= 10^9
 public class MajorityElement {
     public int majorityElement (int[] nums) {
-        // TODO:
-        throw new UnsupportedOperationException();
+        Map<Integer, Integer> map = new HashMap<>();
+        int highestCounter = -1;
+        int result = -1;
+        for (int n : nums) {
+            Integer currentCounter = map.get(n);
+            map.put(n, currentCounter != null ? currentCounter++ : 1);
+
+            currentCounter = map.get(n);
+            if (currentCounter > highestCounter) {
+                highestCounter = currentCounter;
+                result = n;
+            }
+        }
+
+        return result;
     }
 }

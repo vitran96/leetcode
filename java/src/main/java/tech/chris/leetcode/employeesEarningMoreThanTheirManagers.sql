1fc00f49216a8bd5dc07d160439948ade5829ed2
vitran96 <tranuytrieuvi@gmail.com>
2022-10-07 06:57:54 +0700

    employees earning more than their managers - solution

---
+++ java/src/main/java/tech/chris/leetcode/employeesEarningMoreThanTheirManagers.sql
@@ -0,0 +1,26 @@
+-- Table Employee
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | name        | varchar |
+-- | salary      | int     |
+-- | managerId   | int     |
+-- +-------------+---------+
+-- id is the primary key column for this table.
+-- Each row of this table indicates the ID of an employee, their name, salary, and the ID of their manager.
+--
+-- Write an SQL query to find the employees who earn more than their managers.
+-- Return the result table in any order.
+-- The query result format is in the following example.
+--
+-- I will assume that we will compare with there direct manager, not manager of manager
+
+-- Default implementation with join
+SELECT
+    E.name AS 'Employee'
+FROM Employee AS E
+INNER JOIN Employee as M
+ON E.managerId = M.id AND E.salary > M.salary
+
+-- Fastest implementation from online

1c75ff7373513e03b91a681ecc231b5ba1a3910e
vitran96 <tranuytrieuvi@gmail.com>
2022-10-13 05:55:43 +0700

    minor refactor

--- java/src/test/java/tech/chris/leetcode/ExcelSheetColumnNumberTest.java
+++ java/src/test/java/tech/chris/leetcode/ExcelSheetColumnNumberTest.java
@@ -2,19 +2,26 @@ package tech.chris.leetcode;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 public class ExcelSheetColumnNumberTest {
+    static Stream<Arguments> provideArgument () {
+        return Stream.of(Arguments.of("A", 1), Arguments.of("AB", 28), Arguments.of("ZY", 701), Arguments.of("AA", 27),
+                         Arguments.of("ZZZ", 18278), Arguments.of("AAK", 713));
+    }
 
     @ParameterizedTest
-    @CsvSource({"A,1", "AB,28", "ZY,701", "AA,27", "ZZZ,18278", "AAK,713"})
+    @MethodSource("provideArgument")
     public void test1 (String input, int expected) {
         Assertions.assertEquals(expected,
                                 new tech.chris.leetcode.excelSheetColumnNumber.MySolution().titleToNumber(input));
     }
 
     @ParameterizedTest
-    @CsvSource({"A,1", "AB,28", "ZY,701", "AA,27", "ZZZ,18278", "AAK,713"})
+    @MethodSource("provideArgument")
     public void test2 (String input, int expected) {
         Assertions.assertEquals(expected,
                                 new tech.chris.leetcode.excelSheetColumnNumber.OptimizedSolution().titleToNumber(

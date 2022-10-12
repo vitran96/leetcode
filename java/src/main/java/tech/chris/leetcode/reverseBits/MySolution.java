a6504618d576d5e44193177c48dbd6d5f2d2c306
vitran96 <tranuytrieuvi@gmail.com>
2022-10-13 06:52:35 +0700

    reverse bits - solution

--- java/src/main/java/tech/chris/leetcode/reverseBits/MySolution.java
+++ java/src/main/java/tech/chris/leetcode/reverseBits/MySolution.java
@@ -1,8 +1,14 @@
 package tech.chris.leetcode.reverseBits;
 
 public class MySolution implements ReverseBits {
+    // TODO: can be optimized with bitwise operation
     @Override
     public int reverseBits (int n) {
-        return 0;
+        StringBuilder sb = new StringBuilder(Integer.toUnsignedString(n, 2));
+        while (sb.length() < 32) {
+            sb.insert(0, '0');
+        }
+        sb.reverse();
+        return Integer.parseUnsignedInt(sb.toString(), 2);
     }
 }

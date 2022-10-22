65d3bb87dc2dc8271d9e84c10609f65edaa1fee8
vitran96 <tranuytrieuvi@gmail.com>
2022-10-22 07:57:21 +0700

    tenth line - looping solution

--- bash/tenthLine.sh
+++ bash/tenthLine.sh
@@ -3,10 +3,22 @@
 # NOTHING (WTF!! There is a unit test but we have to run to know what we should output :v. Anyway, empty might be the best option here)
 # 2. There's at least three different solutions. Try to explore all possibilities.
 
+# WRONG
 # head -10 file.txt | tail -1
 
-# Solution 2
+# Solution 1
 awk "NR==10" file.txt
 
-# Solution 3
+# Solution 2
 sed -n '10p' file.txt
+
+# Solution 3
+c=0
+while read line && [ $c -le 10 ]
+do
+    c=$((c+1))
+    if [ $c -eq 10 ]
+    then
+        echo $line
+    fi
+done < file.txt

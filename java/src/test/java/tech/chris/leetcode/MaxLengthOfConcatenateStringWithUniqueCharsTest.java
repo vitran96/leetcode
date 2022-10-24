6d977ce91ecdb28e4e9ce494a2c19bb809ac9d02
vitran96 <tranuytrieuvi@gmail.com>
2022-10-24 17:53:10 +0700

    max length of concatenate string with unique chars - init

--- java/src/test/java/tech/chris/leetcode/MaxLengthOfConcatenateStringWithUniqueCharsTest.java
+++ java/src/test/java/tech/chris/leetcode/MaxLengthOfConcatenateStringWithUniqueCharsTest.java
@@ -5,18 +5,37 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class MaxLengthOfConcatenateStringWithUniqueCharsTest {
     static Stream<Arguments> provideArgument () {
-        return Stream.of(Arguments.of(new String[]{"un", "iq", "ue"}, 4),
-                         Arguments.of(new String[]{"cha", "r", "act", "ers"}, 6),
-                         Arguments.of(new String[]{"abcdefghijklmnopqrstuvwxyz"}, 26));
+        return Stream.of(Arguments.of(new ArrayList<String>() {{
+            add("un");
+            add("iq");
+            add("ue");
+        }}, 4), Arguments.of(new ArrayList<String>() {{
+            add("cha");
+            add("r");
+            add("act");
+            add("ers");
+        }}, 6), Arguments.of(new ArrayList<String>() {{
+            add("abcdefghijklmnopqrstuvwxyz");
+        }}, 26), Arguments.of(new ArrayList<String>() {{
+            add("cha");
+            add("act");
+        }}, 3), Arguments.of(new ArrayList<String>() {{
+            add("ccha");
+            add("cact");
+        }}, 0));
     }
 
     @ParameterizedTest
     @MethodSource("provideArgument")
-    public void test1 (int input, int expected) {
-        Assertions.assertEquals(expected, new tech.chris.leetcode.numberOf1Bits.MySolution().hammingWeight(input));
+    public void test1 (List<String> input, int expected) {
+        Assertions.assertEquals(expected,
+                                new tech.chris.leetcode.maxLengthOfConcatenateStringWithUniqueChars.MySolution().maxLength(
+                                    input));
     }
 }

41b171d76a8fa88e4bbf025784f2a60ea9ce763d
vitran96 <tranuytrieuvi@gmail.com>
2022-10-26 07:24:37 +0700

    check if 2 string arrays are equivalent - solution

--- java/src/main/java/tech/chris/leetcode/checkIf2StringArraysAreEquivalent/MySolution.java
+++ java/src/main/java/tech/chris/leetcode/checkIf2StringArraysAreEquivalent/MySolution.java
@@ -1,8 +1,12 @@
 package tech.chris.leetcode.checkIf2StringArraysAreEquivalent;
 
+import java.util.Arrays;
+
 public class MySolution implements CheckIf2StringArraysAreEquivalent {
     @Override
     public boolean arrayStringsAreEqual (String[] word1, String[] word2) {
-        return false;
+        String w1 = Arrays.stream(word1).reduce("", (a, b) -> a + b);
+        String w2 = Arrays.stream(word2).reduce("", (a, b) -> a + b);
+        return w1.equals(w2);
     }
 }

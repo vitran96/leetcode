ac58bee1e358c8d519ce063aa75656b8bd29906c
vitran96 <tranuytrieuvi@gmail.com>
2022-10-31 00:19:13 +0700

    shortest path in grid with wall destroy - solution

--- java/src/main/java/tech/chris/leetcode/shortestPathInGridWithWallDestroy/MySolution.java
+++ java/src/main/java/tech/chris/leetcode/shortestPathInGridWithWallDestroy/MySolution.java
@@ -1,9 +1,6 @@
 package tech.chris.leetcode.shortestPathInGridWithWallDestroy;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class MySolution implements ShortestPathInGridWithWallDestroy {
     private static final List<Position> MOVE_SET = new ArrayList<Position>() {{
@@ -13,64 +10,81 @@ public class MySolution implements ShortestPathInGridWithWallDestroy {
         add(new Position(0, -1));
     }};
     private final Set<Position> visited = new HashSet<>();
+    private List<MovingPosition> currentLevelNodes = new LinkedList<>();
+    private int level = 0;
     private int[][] grid;
-    private int hammer;
-    private Position goal;
-    private STATES state = STATES.RUNNING;
 
     @Override
     public int shortestPath (int[][] grid, int k) {
         this.grid = grid;
-        this.hammer = k;
 
-        goal = new Position(grid[0].length - 1, grid.length - 1);
-        if (goal.x == 0 && goal.y == 0) {
+        Position goal = new Position(grid[0].length - 1, grid.length - 1);
+        MovingPosition startPos = new MovingPosition(0, 0, k);
+        if (goal.equals(startPos)) {
             return 0;
         }
-        shortestPath(new Position(0, 0));
-        return visited.size() - 1;
-    }
-
-    private void shortestPath (Position newPosition) {
-        if (canNotVisit(newPosition)) {
-            return;
-        }
-
-        visited.add(newPosition);
-        hammer -= grid[newPosition.y][newPosition.x];
-        if (newPosition.equals(goal)) {
-            state = STATES.DONE;
-        }
-
-        for (Position move : MOVE_SET) {
-            shortestPath(newPosition.move(move));
-            if (state != STATES.RUNNING) {
-                break;
+        visited.add(startPos);
+        currentLevelNodes.add(startPos);
+        while (currentLevelNodes.size() != 0) {
+            List<MovingPosition> nextLevelQueue = new LinkedList<>();
+            level++;
+            for (MovingPosition p : currentLevelNodes) {
+                for (Position m : MOVE_SET) {
+                    MovingPosition next = move(p, m);
+                    if (next == null) {
+                        continue;
+                    } else if (goal.equals(next)) {
+                        return level;
+                    }
+                    visited.add(next);
+                    nextLevelQueue.add(next);
+                }
             }
+            currentLevelNodes = nextLevelQueue;
+        }
+        return -1;
+    }
+
+    private MovingPosition move (MovingPosition p, Position vector) {
+        int x = p.x + vector.x;
+        int y = p.y + vector.y;
+        if (x < 0 || x >= grid[0].length || y < 0 || y >= grid.length || p.hammer - grid[y][x] < 0) {
+            return null;
+        }
+        MovingPosition next = new MovingPosition(x, y, p.hammer - grid[y][x]);
+        return visited.contains(next) ? null : next;
+    }
+
+    private static class MovingPosition extends Position {
+        int hammer;
+
+        MovingPosition (int x, int y, int hammer) {
+            super(x, y);
+            this.hammer = hammer;
         }
 
-        if (state != STATES.DONE) {
-            visited.remove(newPosition);
-            hammer += grid[newPosition.y][newPosition.x];
+        @Override
+        public boolean equals (Object o) {
+            if (this == o) {return true;}
+            if (o == null || getClass() != o.getClass()) {return false;}
+            if (!super.equals(o)) {return false;}
+
+            MovingPosition that = (MovingPosition) o;
+
+            return hammer == that.hammer;
+        }
+
+        @Override
+        public int hashCode () {
+            int result = super.hashCode();
+            result = 31 * result + hammer;
+            return result;
         }
     }
 
-    private boolean canNotVisit (Position p) {
-        return visited.contains(p)
-            || p.x < 0
-            || p.x >= grid[0].length
-            || p.y < 0
-            || p.y >= grid.length
-            || hammer - grid[p.y][p.x] < 0;
-    }
-
-    private enum STATES {
-        RUNNING, STOPPED, DONE
-    }
-
-    private static final class Position {
-        int x = 0;
-        int y = 0;
+    private static class Position {
+        int x;
+        int y;
 
         Position (int x, int y) {
             this.x = x;
@@ -79,8 +93,12 @@ public class MySolution implements ShortestPathInGridWithWallDestroy {
 
         @Override
         public boolean equals (Object o) {
-            if (this == o) {return true;}
-            if (o == null || getClass() != o.getClass()) {return false;}
+            if (this == o) {
+                return true;
+            }
+            if (!(o instanceof Position)) {
+                return false;
+            }
 
             Position position = (Position) o;
 
@@ -98,10 +116,6 @@ public class MySolution implements ShortestPathInGridWithWallDestroy {
         @Override
         public String toString () {
             return "(" + x + "," + y + ")";
-        }
-
-        private Position move (Position m) {
-            return new Position(x + m.x, y + m.y);
         }
     }
 }

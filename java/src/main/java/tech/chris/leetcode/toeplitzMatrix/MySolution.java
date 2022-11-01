47e1ec018bc5a31a5a910cbeeebab65f33d6704a
vitran96 <tranuytrieuvi@gmail.com>
2022-11-01 07:20:33 +0700

    toeplitz matrix - solution

--- java/src/main/java/tech/chris/leetcode/toeplitzMatrix/MySolution.java
+++ java/src/main/java/tech/chris/leetcode/toeplitzMatrix/MySolution.java
@@ -3,6 +3,21 @@ package tech.chris.leetcode.toeplitzMatrix;
 public class MySolution implements ToeplitzMatrix {
     @Override
     public boolean isToeplitzMatrix (int[][] matrix) {
-        return false;
+        int width = matrix[0].length;
+        int height = matrix.length;
+
+        int[] lastRow = matrix[0];
+        for (int i = 1; i < height; i++) {
+            int[] currentRow = matrix[i];
+            for (int j = 0; j < width - 1; j++) {
+                if (lastRow[j] != currentRow[j + 1]) {
+                    return false;
+                }
+            }
+
+            lastRow = currentRow;
+        }
+
+        return true;
     }
 }

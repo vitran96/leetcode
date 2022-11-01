7d3d8ba5ecca1a2bf07a96a5916faa30b2992b51
vitran96 <tranuytrieuvi@gmail.com>
2022-11-01 21:34:57 +0700

    where will the ball fall - solution

--- java/src/main/java/tech/chris/leetcode/whereWillTheBallFall/MySolution.java
+++ java/src/main/java/tech/chris/leetcode/whereWillTheBallFall/MySolution.java
@@ -1,8 +1,30 @@
 package tech.chris.leetcode.whereWillTheBallFall;
 
+import java.util.stream.IntStream;
+
 public class MySolution implements WhereWillTheBallFall {
     @Override
     public int[] findBall (int[][] grid) {
-        return new int[0];
+        int[] result = IntStream.range(0, grid[0].length).toArray();
+
+        for (int[] ints : grid) {
+            for (int j = 0; j < result.length; j++) {
+                if (result[j] == -1) {
+                    continue;
+                }
+
+                int p = result[j];
+                result[j] += ints[p];
+                if (result[j] < 0 || result[j] >= result.length) {
+                    result[j] = -1;
+                } else if (p > 0 && ints[p] == -1 && ints[p - 1] == 1) {
+                    result[j] = -1;
+                } else if (p < ints.length - 1 && ints[p] == 1 && ints[p + 1] == -1) {
+                    result[j] = -1;
+                }
+            }
+        }
+
+        return result;
     }
 }

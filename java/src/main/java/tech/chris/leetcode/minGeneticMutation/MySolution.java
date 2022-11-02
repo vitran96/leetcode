a824176e73612a79b62a5c2c2683244693dc119e
vitran96 <tranuytrieuvi@gmail.com>
2022-11-02 23:29:01 +0700

    min genetic mutation - solution

--- java/src/main/java/tech/chris/leetcode/minGeneticMutation/MySolution.java
+++ java/src/main/java/tech/chris/leetcode/minGeneticMutation/MySolution.java
@@ -1,41 +1,55 @@
 package tech.chris.leetcode.minGeneticMutation;
 
-import java.util.*;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
 
 public class MySolution implements MinGeneticMutation {
     char[] chars = new char[]{'A', 'C', 'G', 'T'};
 
     @Override
     public int minMutation (String start, String end, String[] bank) {
-        Set<String> set = new HashSet<>();
-        Collections.addAll(set, bank);
+        boolean endIsValid = false;
+        for (String item : bank) {
+            if (item.equals(end)) {
+                endIsValid = true;
+                break;
+            }
+        }
 
-        if (!set.contains(end)) {
+        if (!endIsValid) {
             return -1;
         }
 
         int level = 0;
+        Set<String> visited = new HashSet<>();
         List<String> strings = new LinkedList<>();
         strings.add(start);
         while (strings.size() > 0) {
             List<String> nextStrings = new LinkedList<>();
             level++;
             for (String string : strings) {
-                for (char c : chars) {
-                    for (int i = 0; i < string.length(); i++) {
-                        StringBuilder stringBuilder = new StringBuilder(string);
-                        stringBuilder.setCharAt(i, c);
-                        String newString = stringBuilder.toString();
-                        if (newString.equals(end)) {
-                            return level;
-                        }
+                for (String item : bank) {
+                    if (visited.contains(item) || item.equals(string)) {
+                        continue;
+                    }
 
-                        if (set.contains(newString)) {
-                            nextStrings.add(newString);
+                    int count = 0;
+                    for (int i = 0; i < string.length(); i++) {
+                        if (string.charAt(i) != item.charAt(i)) {
+                            count++;
                         }
                     }
 
+                    if (count == 1) {
+                        if (item.equals(end)) {
+                            return level;
+                        }
+                        nextStrings.add(item);
+                    }
                 }
+                visited.add(string);
             }
             strings = nextStrings;
         }

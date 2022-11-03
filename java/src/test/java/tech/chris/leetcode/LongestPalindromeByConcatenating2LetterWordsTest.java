cc3101cbbbb37fffb983fd9575984dab002e7d3a
vitran96 <tranuytrieuvi@gmail.com>
2022-11-04 01:13:27 +0700

    longest palindrome by concatenating 2 letter words - update test

--- java/src/test/java/tech/chris/leetcode/LongestPalindromeByConcatenating2LetterWordsTest.java
+++ java/src/test/java/tech/chris/leetcode/LongestPalindromeByConcatenating2LetterWordsTest.java
@@ -14,6 +14,8 @@ public class LongestPalindromeByConcatenating2LetterWordsTest {
             Arguments.of(new String[]{"lc", "cl", "gg"}, 6),
             Arguments.of(new String[]{"ab", "ty", "yt", "lc", "cl", "ab"}, 8),
             Arguments.of(new String[]{"cc", "ll", "xx"}, 2),
+            Arguments.of(new String[]{"cc", "ll", "xx", "xx"}, 6),
+            Arguments.of(new String[]{"cc", "ll", "xx", "xx", "xx", "xx"}, 10),
             Arguments.of(new String[]{
                 "dd", "aa", "bb", "dd", "aa", "dd", "bb", "dd", "aa", "cc", "bb", "cc", "dd", "cc"
             }, 10)

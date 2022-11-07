226b0dca5da3e13d262890a30ec6971df0d41269
vitran96 <tranuytrieuvi@gmail.com>
2022-11-07 22:05:24 +0700

    max 69 num - add note

--- java/src/main/java/tech/chris/leetcode/max69Num/MySolution1.java
+++ java/src/main/java/tech/chris/leetcode/max69Num/MySolution1.java
@@ -1,5 +1,9 @@
 package tech.chris.leetcode.max69Num;
 
+/**
+ * TODO: This can be optimized by using math to find the 1st '6'
+ * Then use math to replace the '6'
+ */
 public class MySolution1 implements Max69Num {
     @Override
     public int maximum69Number (int num) {

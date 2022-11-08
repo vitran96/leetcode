1a8fe8f612ab8306acd31e6741be3c8398142c55
vitran96 <tranuytrieuvi@gmail.com>
2022-11-08 21:53:49 +0700

    make the string great - my solution

--- java/src/main/java/tech/chris/leetcode/makeTheStringGreat/MySolution.java
+++ java/src/main/java/tech/chris/leetcode/makeTheStringGreat/MySolution.java
@@ -3,6 +3,24 @@ package tech.chris.leetcode.makeTheStringGreat;
 public class MySolution implements MakeTheStringGreat {
     @Override
     public String makeGood (String s) {
-        return null;
+        StringBuilder sb = new StringBuilder(s);
+        while (true) {
+            boolean isGood = true;
+            for (int i = 0; i < sb.length() - 1; i++) {
+                char c1 = sb.charAt(i);
+                char c2 = sb.charAt(i + 1);
+                if (c1 != c2 && (
+                    Character.toLowerCase(c1) == c2 || c1 == Character.toLowerCase(c2)
+                )) {
+                    isGood = false;
+                    sb.replace(i, i + 2, "");
+                }
+            }
+
+            if (isGood) {
+                break;
+            }
+        }
+        return sb.toString();
     }
 }

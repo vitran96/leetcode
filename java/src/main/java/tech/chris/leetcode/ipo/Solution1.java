9fbbf74ad7ebd6cf2cada0ccf098e4794796c6b9
vitran96 <tranuytrieuvi@gmail.com>
2023-02-23 13:52:26 +0700

    ipo - solution 1

---
+++ java/src/main/java/tech/chris/leetcode/ipo/Solution1.java
@@ -0,0 +1,64 @@
+package tech.chris.leetcode.ipo;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+/**
+ * Greedy approach
+ * We sort by Profit
+ */
+public class Solution1 implements Ipo {
+    @Override
+    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+        ArrayList<Project> projects = new ArrayList<>(profits.length);
+        for (int i = 0; i < profits.length; i++) {
+            projects.add(new Project(profits[i], capital[i]));
+        }
+        projects.sort(Comparator.reverseOrder());
+
+        while (k > 0) {
+            if (projects.size() == 0) {
+                break;
+            }
+
+            int selected = -1;
+            for (int i = 0; i < projects.size(); i++) {
+                if (projects.get(i).capital <= w) {
+                    selected = i;
+                    break;
+                }
+            }
+
+            if (selected < 0) {
+                break;
+            }
+
+            w += projects.get(selected).profit;
+            k--;
+        }
+        return w;
+    }
+
+    class Project implements Comparable<Project> {
+        int profit;
+        int capital;
+
+        public Project(int profit, int capital) {
+            this.profit = profit;
+            this.capital = capital;
+        }
+
+        @Override
+        public String toString() {
+            return "Project{" +
+                "profit=" + profit +
+                ", capital=" + capital +
+                '}';
+        }
+
+        @Override
+        public int compareTo(Project o) {
+            return profit - o.profit;
+        }
+    }
+}

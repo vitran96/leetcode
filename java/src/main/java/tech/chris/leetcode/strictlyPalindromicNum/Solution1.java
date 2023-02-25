b7b302d1b432afde58a35d5ef9a214ca084aeb0e
vitran96 <tranuytrieuvi@gmail.com>
2023-02-26 06:43:49 +0700

    strictly palindromic num - init

---
+++ java/src/main/java/tech/chris/leetcode/strictlyPalindromicNum/Solution1.java
@@ -0,0 +1,8 @@
+package tech.chris.leetcode.strictlyPalindromicNum;
+
+public class Solution1 implements StrictlyPalindromicNum {
+    @Override
+    public boolean isStrictlyPalindromic(int n) {
+        return false;
+    }
+}

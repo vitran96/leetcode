b8e9b4ae1ac057c2ff00c98863b4ef18c2d0f039
vitran96 <tranuytrieuvi@gmail.com>
2023-02-27 21:21:17 +0700

    construct quad tree - solution 1

--- java/src/main/java/tech/chris/leetcode/constructQuadTree/Solution1.java
+++ java/src/main/java/tech/chris/leetcode/constructQuadTree/Solution1.java
@@ -6,22 +6,22 @@ public class Solution1 implements ConstructQuadTree {
 
     @Override
     public Node construct(int[][] grid) {
-        return construct(grid, 0, 0);
+        return construct(grid, 0, 0, grid.length - 1, grid.length - 1);
     }
 
 
-    private Node construct(int[][] grid, int nodeX, int nodeY) {
-        Node current = new Node(grid[nodeY][nodeX] == 1, true);
-        int x = nodeX + 1;
-        int y = nodeY + 1;
-        if (x >= grid.length || y >= grid.length) {
+    private Node construct(int[][] grid, int colStart, int rowStart, int colEnd, int rowEnd) {
+        Node current = new Node(grid[rowStart][colStart] == 1, true);
+        if (colStart >= grid.length || rowStart >= grid.length) {
             return current;
         }
 
+        int x = colStart + 1;
+        int y = rowStart + 1;
         boolean hasDiff = false;
-        while (true) {
-            for (int i = nodeY; i <= y; i++) {
-                if ((grid[i][nodeX] == 1) != current.val) {
+        while (x <= colEnd || y <= rowEnd) {
+            for (int i = rowStart; i <= y; i++) {
+                if ((grid[i][colStart] == 1) != current.val) {
                     hasDiff = true;
                     break;
                 }
@@ -31,18 +31,14 @@ public class Solution1 implements ConstructQuadTree {
                 break;
             }
 
-            for (int i = nodeX; i <= x; i++) {
-                if ((grid[nodeY][i] == 1) != current.val) {
+            for (int i = colStart; i <= x; i++) {
+                if ((grid[rowStart][i] == 1) != current.val) {
                     hasDiff = true;
                     break;
                 }
             }
 
             if (hasDiff) {
-                break;
-            }
-
-            if (x + 1 == grid.length || y + 1 == grid.length) {
                 break;
             }
 
@@ -55,9 +51,9 @@ public class Solution1 implements ConstructQuadTree {
             current.isLeaf = false;
             current.val = true;
 
-            current.topRight = construct(grid, x, nodeY);
-            current.bottomLeft = construct(grid, nodeX, y);
-            current.bottomRight = construct(grid, x, y);
+            current.topRight = construct(grid, x, rowStart, colEnd, y - 1);
+            current.bottomLeft = construct(grid, colStart, y, x - 1, rowEnd);
+            current.bottomRight = construct(grid, x, y, colEnd, rowEnd);
         }
         return current;
     }

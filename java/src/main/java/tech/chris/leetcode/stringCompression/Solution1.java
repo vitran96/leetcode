b60aefcfd03d0dc1c7e247ad4af8cfa45e7785ce
vitran96 <tranuytrieuvi@gmail.com>
2023-03-02 19:50:01 +0700

    string compression - init

---
+++ java/src/main/java/tech/chris/leetcode/stringCompression/Solution1.java
@@ -0,0 +1,8 @@
+package tech.chris.leetcode.stringCompression;
+
+public class Solution1 implements StringCompression {
+    @Override
+    public int compare(char[] chars) {
+        return 0;
+    }
+}
---
+++ java/src/test/java/tech/chris/leetcode/StringCompressionTest.java
@@ -0,0 +1,27 @@
+package tech.chris.leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import tech.chris.leetcode.stringCompression.Solution1;
+
+import java.util.stream.Stream;
+
+public class StringCompressionTest {
+
+    static Stream<Arguments> provideArgument() {
+        return Stream.of(
+            Arguments.of(new char[]{'a', 'a', 'b', 'b', 'c', 'c', 'c'}, 6),
+            Arguments.of(new char[]{'a'}, 1),
+            Arguments.of(new char[]{'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'},
+                         4)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArgument")
+    public void test(char[] input, int expected) {
+        Assertions.assertEquals(expected, new Solution1().compare(input));
+    }
+}

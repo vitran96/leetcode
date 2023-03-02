4377f3632992c14bdcb4a2d9c6341bc74458734a
vitran96 <tranuytrieuvi@gmail.com>
2023-03-02 21:32:53 +0700

    sort an array - solution 1 - failed

--- java/src/main/java/tech/chris/leetcode/sortAnArray/Solution1.java
+++ java/src/main/java/tech/chris/leetcode/sortAnArray/Solution1.java
@@ -1,8 +1,36 @@
 package tech.chris.leetcode.sortAnArray;
 
+import java.util.Arrays;
+
 public class Solution1 implements SortAnArray {
     @Override
     public int[] sortArray(int[] nums) {
-        return new int[0];
+        int[] result = Arrays.copyOf(nums, nums.length);
+        quickSort(result, 0, result.length - 1);
+        return result;
+    }
+
+    public void quickSort(int[] nums, int l, int r) {
+        if (l >= r) {
+            return;
+        }
+
+        int pivot = nums[r];
+        int j = l - 1;
+        for (int i = l; i <= r - 1; i++) {
+            if (nums[i] < pivot) {
+                j++;
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+        }
+
+        int temp = nums[j + 1];
+        nums[j + 1] = pivot;
+        nums[r] = temp;
+
+        quickSort(nums, l, j);
+        quickSort(nums, j + 2, r);
     }
 }
--- java/src/main/java/tech/chris/leetcode/sortAnArray/SortAnArray.java
+++ java/src/main/java/tech/chris/leetcode/sortAnArray/SortAnArray.java
@@ -1,5 +1,11 @@
 package tech.chris.leetcode.sortAnArray;
 
+// Sort asc-order
+// Time: O(n log(n))
+// Space: smallest possible
+// Constraints:
+// * 1<=nums.length<=5*10^4
+// * -5*10^4<=nums[i]<=5*10^4
 public interface SortAnArray {
     int[] sortArray(int[] nums);
 }

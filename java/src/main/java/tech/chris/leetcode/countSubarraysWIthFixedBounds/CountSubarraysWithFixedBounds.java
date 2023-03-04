be11efc917d7dc1dfdaa32005ac8c53d67946c6d
vitran96 <tranuytrieuvi@gmail.com>
2023-03-04 10:03:37 +0700

    count subarrays with fixed bounds - cannot find a good way to solve :(

--- java/src/main/java/tech/chris/leetcode/countSubarraysWIthFixedBounds/CountSubarraysWithFixedBounds.java
+++ java/src/main/java/tech/chris/leetcode/countSubarraysWIthFixedBounds/CountSubarraysWithFixedBounds.java
@@ -1,5 +1,15 @@
 package tech.chris.leetcode.countSubarraysWIthFixedBounds;
 
+// You are given an integer array nums and two integers minK and maxK.
+// A fixed-bound subarray of nums is a subarray that satisfies the following conditions:
+// * The minimum value in the subarray is equal to minK.
+// * The maximum value in the subarray is equal to maxK.
+// Return the number of fixed-bound subarrays.
+// A subarray is a contiguous part of an array.
+//
+// Constraints:
+// * 2<=nums.length<=105
+// * 1<=nums[i],minK,maxK<=106
 public interface CountSubarraysWithFixedBounds {
     long countSubarrays(int[] nums, int minK, int maxK);
 }
--- java/src/main/java/tech/chris/leetcode/countSubarraysWIthFixedBounds/Solution1.java
+++ java/src/main/java/tech/chris/leetcode/countSubarraysWIthFixedBounds/Solution1.java
@@ -1,8 +1,49 @@
 package tech.chris.leetcode.countSubarraysWIthFixedBounds;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Solution1 implements CountSubarraysWithFixedBounds {
     @Override
     public long countSubarrays(int[] nums, int minK, int maxK) {
+        int result = 0;
+
+        List<Integer> minKIndexes = new LinkedList<>();
+        List<Integer> maxKIndexes = new LinkedList<>();
+        int l = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < minK || nums[i] > maxK) {
+                if (minKIndexes.size() > 0 && maxKIndexes.size() > 0) {
+                    //
+                }
+
+                minKIndexes.clear();
+                maxKIndexes.clear();
+                l = i + 1;
+            }
+
+            if (nums[i] == minK) {
+                minKIndexes.add(nums[i]);
+            } else if (nums[i] == maxK) {
+                maxKIndexes.add(nums[i]);
+            }
+        }
+
+        if (l < nums.length && minKIndexes.size() > 0 && maxKIndexes.size() > 0) {
+            //
+        }
+
+        return result;
+    }
+
+    private long countSubarrays(int[] nums,
+                                int l,
+                                int r,
+                                List<Integer> minKIndexes,
+                                List<Integer> maxKIndexes) {
+        int lowestKIndex = Math.min(minKIndexes.get(0), maxKIndexes.get(0));
+        int highestKIndex = Math.max(minKIndexes.get(minKIndexes.size() - 1), maxKIndexes.get(
+            maxKIndexes.size() - 1));
         return 0;
     }
 }

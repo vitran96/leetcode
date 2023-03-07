44d050bebb982d3bea620673ad75590e4bb6abce
vitran96 <tranuytrieuvi@gmail.com>
2023-03-07 13:36:51 +0700

    min time 2 complete trips - init

---
+++ java/src/main/java/tech/chris/leetcode/minTime2CompleteTrips/Solution1.java
@@ -0,0 +1,8 @@
+package tech.chris.leetcode.minTime2CompleteTrips;
+
+public class Solution1 implements MinTime2CompleteTrips {
+    @Override
+    public long minimumTime(int[] time, int totalTrips) {
+        return 0;
+    }
+}
---
+++ java/src/test/java/tech/chris/leetcode/MinTime2CompleteTripsTest.java
@@ -0,0 +1,25 @@
+package tech.chris.leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import tech.chris.leetcode.minTime2CompleteTrips.Solution1;
+
+import java.util.stream.Stream;
+
+public class MinTime2CompleteTripsTest {
+
+    static Stream<Arguments> provideArgument() {
+        return Stream.of(
+            Arguments.of(new int[]{1, 2, 3}, 5, 3),
+            Arguments.of(new int[]{2}, 1, 2)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArgument")
+    public void test(int[] time, int totalTrips, int expected) {
+        Assertions.assertEquals(expected, new Solution1().minimumTime(time, totalTrips));
+    }
+}

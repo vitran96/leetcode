646b026183c070b85502e2e9f3bfde91babc74df
vitran96 <tranuytrieuvi@gmail.com>
2023-03-09 13:02:51 +0700

    run sum of 1d array - solution 1

--- java/src/main/java/tech/chris/leetcode/runSumOf1DArray/Solution1.java
+++ java/src/main/java/tech/chris/leetcode/runSumOf1DArray/Solution1.java
@@ -3,6 +3,12 @@ package tech.chris.leetcode.runSumOf1DArray;
 public class Solution1 implements RunSumOf1DArray {
     @Override
     public int[] runningSum(int[] nums) {
-        return new int[0];
+        int[] result = new int[nums.length];
+        int currentSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            currentSum += nums[i];
+            result[i] = currentSum;
+        }
+        return result;
     }
 }

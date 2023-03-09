a4e43ac689a376b85195375fb82492e3f6fa6b20
vitran96 <tranuytrieuvi@gmail.com>
2023-03-09 14:23:42 +0700

    linked list cycle 2 - solution 1

--- java/src/main/java/tech/chris/leetcode/linkedListCycle2/Solution1.java
+++ java/src/main/java/tech/chris/leetcode/linkedListCycle2/Solution1.java
@@ -2,9 +2,21 @@ package tech.chris.leetcode.linkedListCycle2;
 
 import tech.chris.leetcode.ListNode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution1 implements LinkedListCycle2 {
     @Override
     public ListNode detectCycle(ListNode head) {
-        return null;
+        Set<ListNode> set = new HashSet<>();
+        ListNode current = head;
+        while (current != null) {
+            if (!set.add(current)) {
+                break;
+            }
+
+            current = current.next;
+        }
+        return current;
     }
 }

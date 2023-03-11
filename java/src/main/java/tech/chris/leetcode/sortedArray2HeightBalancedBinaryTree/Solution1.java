7122804d4f251627282e3031c1ef07cbc3538c18
vitran96 <tranuytrieuvi@gmail.com>
2023-03-11 15:26:55 +0700

    sorted array 2 hieght balanced binary tree - solution 1

--- java/src/main/java/tech/chris/leetcode/sortedArray2HeightBalancedBinaryTree/Solution1.java
+++ java/src/main/java/tech/chris/leetcode/sortedArray2HeightBalancedBinaryTree/Solution1.java
@@ -3,9 +3,38 @@ package tech.chris.leetcode.sortedArray2HeightBalancedBinaryTree;
 import tech.chris.leetcode.ListNode;
 import tech.chris.leetcode.TreeNode;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Solution1 implements SortedArray2HeightBalancedBinaryTree {
     @Override
     public TreeNode sortedListToBST(ListNode head) {
-        return null;
+        if (head == null) {
+            return null;
+        }
+
+        List<TreeNode> list = new LinkedList<>();
+        ListNode current = head;
+        while (current != null) {
+            list.add(new TreeNode(current.val));
+            current = current.next;
+        }
+
+        return convert(list, 0, list.size() - 1);
+    }
+
+    private TreeNode convert(List<TreeNode> list, int l, int r) {
+        if (l > r) {
+            return null;
+        }
+
+        int mid = l + r;
+        mid = mid % 2 == 0 ? mid / 2 : mid / 2 + 1;
+
+        TreeNode head = list.get(mid);
+        head.left = convert(list, l, mid - 1);
+        head.right = convert(list, mid + 1, r);
+
+        return head;
     }
 }

d238f1160684ce0f50ea2c7d588f4a00d02c48f5
vitran96 <tranuytrieuvi@gmail.com>
2023-03-11 15:34:00 +0700

    sorted array 2 hieght balanced binary tree - add document

--- java/src/main/java/tech/chris/leetcode/sortedArray2HeightBalancedBinaryTree/SortedArray2HeightBalancedBinaryTree.java
+++ java/src/main/java/tech/chris/leetcode/sortedArray2HeightBalancedBinaryTree/SortedArray2HeightBalancedBinaryTree.java
@@ -3,6 +3,16 @@ package tech.chris.leetcode.sortedArray2HeightBalancedBinaryTree;
 import tech.chris.leetcode.ListNode;
 import tech.chris.leetcode.TreeNode;
 
+/**
+ * Given the head of a singly linked list, return the middle node of the linked list.
+ * If there are two middle nodes, return the second middle node.
+ * <p>
+ * Constraints:
+ * <ul>
+ *     <li>The number of nodes in the list is in the range [1, 100].</li>
+ *     <li>1 <= Node.val <= 100</li>
+ * </ul>
+ */
 public interface SortedArray2HeightBalancedBinaryTree {
     TreeNode sortedListToBST(ListNode head);
 }
